793cbe23914ba49fc954133521e32ee3c6ec953f
GTmhyu <zmaula10@gmail.com>
2023-11-09 17:24:27 +0700

    Json AI + Chatbot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import Canvas from "./canvas";
 import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
+import Chat from "./chat";
 
 function App() {
 
   return (
     <main className="app transition-all ease-in">
-      
+      <Chat />
       <Home />
       <Canvas />
       <Customizer />
